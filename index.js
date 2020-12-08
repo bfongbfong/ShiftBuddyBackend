fae77152aa09d9c8665d1ae9c567e5032d45c207
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2020-12-07 21:37:04 -0500

    Begins writing shift routes

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const createError = require('http-errors');
 
+const shiftRoutes = require('./routes/shifts');
+
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // const config = require('config'); // taken from rz
@@ -15,6 +17,7 @@ app.get("/", (req, res) => {
     res.send("HI WELCOME TO MY WEBSITE");
 })
 
+app.use('/shifts', shiftRoutes);
 
 app.listen(3000, () => {
     console.log("serving app on localhost:3000")
