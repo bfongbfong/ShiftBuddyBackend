a86d169b61fa9b19e3a240094abd48bce97ceafe
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2020-12-07 21:04:10 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const createError = require('http-errors');
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// const config = require('config'); // taken from rz
+const cors = require('cors'); // taken from rz, not sure what it does
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("HI WELCOME TO MY WEBSITE");
+})
+
+
+app.listen(3000, () => {
+    console.log("serving app on localhost:3000")
+})
