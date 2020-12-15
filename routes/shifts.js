9fa6689e692e64bd002963bdbb621fcb1834ae2c
Brandon Fong <50242218+bfongbfong@users.noreply.github.com>
2020-12-15 09:40:12 -0500

    Creates route to create a new shift

--- routes/shifts.js
+++ routes/shifts.js
@@ -1,9 +1,48 @@
 const express = require('express');
+const { Mongoose } = require('mongoose');
 const router = express.Router();
+const { body: check, validationResult } = require('express-validator');
+
+const shiftModel = require('../models/shift');
 
 router.get('/:user_id', (req, res) => {
     const { user_id } = req.params;
     return res.json({"name": "brandon", "age": 24, "user_id": user_id});
-})
+});
+
+const emptyErrMsgSuffix = ' must be provided.'
+router.post('/', [
+    check('type').not().isEmpty().withMessage('Type' + emptyErrMsgSuffix),
+    check('length').not().isEmpty().withMessage('Length' + emptyErrMsgSuffix),
+    check('openForTrade').not().isEmpty().withMessage('OpenForTrade' + emptyErrMsgSuffix),
+    check('status').not().isEmpty().withMessage('Status' + emptyErrMsgSuffix),
+    check('date').not().isEmpty().withMessage('Date' + emptyErrMsgSuffix),
+    check('group').not().isEmpty().withMessage('Group' + emptyErrMsgSuffix),
+], (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        const errs = errors.array();
+        return res.status(400).json({ errorMessage: errs[0].msg });
+    }
+    
+    const shift = new shiftModel({
+        user: req.body.user,
+        type: req.body.type,
+        length: req.body.length,
+        openForTrade: req.body.openForTrade,
+        status: req.body.status,
+        date: req.body.date,
+        group: req.body.group
+    });
+
+    shift.save()
+    .then(shift => {
+        return res.json({ shift });
+    })
+    .catch(err => {
+        const errorMsg = err.message || constants.UKNOWN_ERROR;
+        return res.status(err.code || 500).json({ errorMessage: errorMsg });    });
+});
 
 module.exports = router;
